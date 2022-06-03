de440dbe0d05c36108f4c2a693585ac3aa962462
Hugo LAGAHE <lagahehugo@gmail.com>
2022-06-03 15:06:45 +0200

    ajout connexion à Metamask

---
+++ Site/JS/connexionMetamask.js
@@ -0,0 +1,20 @@
+async function connectMetaMask(){
+
+    if(typeof window.ethereum !== "undefined"){
+        try
+        {
+            await ethereum.request({ method: "eth_requestAccounts" });
+        }
+        catch (error) {
+            console.log(error);
+        }
+       
+        document.getElementById("connectButton").innerHTML = "Connected";
+        
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        console.log(accounts);
+    }
+    else {
+        document.getElementById("connectButton").innerHTML ="Please install MetaMask";
+      }
+}
