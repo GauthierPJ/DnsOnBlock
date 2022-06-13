ec91ce85a25d81a10e22d8e125aed809c3f4af0b
Hugo LAGAHE <lagahehugo@gmail.com>
2022-06-13 17:18:36 +0200

    ajout connexion metamask à express js

---
+++ SiteExpress/index.js
@@ -0,0 +1,137 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var session = require('express-session');
+
+// var connexionMeta = require('./connexionMetamask.js');
+
+var app = module.exports = express();
+
+// config
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret'
+}));
+
+// Session-persisted message middleware
+
+app.use(function(req, res, next){
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
+});
+
+app.use(express.static(__dirname + '/public/'));
+
+// dummy database
+
+var users = {
+  tj: { name: 'tj' }
+};
+
+
+// Authenticate using our plain-object database of doom!
+
+function authenticate(fn) {
+  
+  var user = connexion();
+  console.log("okkkk");
+  // query the db for the given username
+  if (!user) return fn(null, null)
+  // apply the same algorithm to the POSTed password, applying
+  // the hash against the pass / salt, if there is a match we
+  // found the user
+  return fn(null, user);
+}
+
+
+function connexion() {
+  
+  const account=1;
+  return account;
+}
+
+function restrict(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+app.get('/', function(req, res){
+  console.log("get");
+  res.redirect('/login');
+});
+
+app.get('/restricted', restrict, function(req, res){
+  res.send('Wahoo! restricted area, click to <a href="/logout">logout</a>');
+});
+
+app.get('/logout', function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/');
+  });
+});
+
+app.get('/login', function(req, res){
+  res.render('login');
+});
+
+app.post('/login', function (req, res, next) {
+  // après avoir appuyé sur login
+  
+  console.log("authenticate"+req.body.account);
+  authenticate(function(err, user){
+    if (err) return next(err)
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        // initialise la session
+        req.session.user = user;
+        console.log("affichage");
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+        console.log(req.session.success);
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
+    }
+  });
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
---
+++ SiteExpress/public/JS/connexionMetamask.js
@@ -0,0 +1,33 @@
+
+
+async function connectMetaMask(){
+
+    if(typeof window.ethereum !== "undefined"){
+        try
+        {
+            await ethereum.request({ method: "eth_requestAccounts" });
+        }
+        catch (error) {
+            console.log(error);
+        }
+        document.getElementById("connectButton").innerHTML = "Connected";
+
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        console.log("accounts"+accounts);
+        document.getElementById("adressePublique").innerHTML = accounts;
+        console.log(accounts);
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/login', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                // Request finished. Do processing here.
+            }
+        }
+        xhr.send("account="+accounts);
+    }
+    else {
+        document.getElementById("connectButton").innerHTML ="Please install MetaMask";
+      }
+}
